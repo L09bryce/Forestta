5502c95ca12b93ae9f5a4acb3bc3e07127520902
Bryce <gichuhibryson09@gmail.com>
2025-01-19 23:38:17 +0300

    More on hero component

--- Forestta/src/Components/Hero.jsx
+++ Forestta/src/Components/Hero.jsx
@@ -11,8 +11,11 @@ const Hero = () => {
        </small>
        <div className='ml-5 mb-5' id='hero'>
         <p>
-            <div className='text-warning fw-bold fs-1'>SAVOUR THE</div> <br/> <span>freshness</span>
+            <div className='text-warning fw-bold fs-1'>SAVOUR THE</div> <span className='fw-100'>freshness</span>
         </p>
+       </div>
+       <div>
+        <i className="ri-arrow-down-line h-75%" id='icon'></i>
        </div>
     </div>
   )
