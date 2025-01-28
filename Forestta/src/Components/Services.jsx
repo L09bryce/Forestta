acfdb96021cbc527d123e74c583d1db23fac9347
Bryce <gichuhibryson09@gmail.com>
2025-01-28 22:06:21 +0300

    Service component updated

--- Forestta/src/Components/Services.jsx
+++ Forestta/src/Components/Services.jsx
@@ -5,30 +5,26 @@ import './Services.css'
 
 const Services = () => {
     const targetRef = useRef(null);
-    const {scrollXProgress} = useScroll({ target: targetRef});
-    const x = useTransform(scrollXProgress, [0, 1], ['0', '-55'])
+    const {scrollYProgress} = useScroll({ target: targetRef});
+    const x = useTransform(scrollYProgress, [0, 2], ['100%', '-100%'])
   return (
-    <div className='services'>
-        <div className='offered' ref={targetRef}>
-            <motion.div className='container' style={{x}}><p>-PREMIUM COFFEE -FRESH PRODUCE -RELISHING FLAVOURS</p></motion.div>
+    <div className='services' ref={targetRef}>
+        <div className='offered' >
+            <motion.div className='container' style={{x,}}><p>-PREMIUM COFFEE -FRESH PRODUCE -RELISHING COFFEE</p></motion.div>
         </div>
-        <div className="card1">
+        <div className="cards mt-5">
              <div className="card d-flex" style={{backgroundColor: 'green'}}>
             <div className="row">
                 <div className='col-5 fs-25'><p>FRESH PRODUCE <br/><span>LOCALLY PRODUCED</span></p></div>
             <div className='col-7'><img src="Item1.jpg" alt="" /></div>
             </div>
         </div>
-        </div>
-        <div className="card2">
             <div className="card d-flex" style={{backgroundColor: 'greenyellow'}}>
             <div className="row">
                 <div className='col-5 fs-25'><p>CULINARY DELIGHTS <br/><span>FRESH FLAVOURS</span></p></div>
             <div className='col-7'><img src="Item5.webp" alt="" /></div>
             </div>
         </div>
-        </div>
-        <div className="card3">
             <div className="card d-flex" style={{backgroundColor: 'brown'}}>
             <div className="row">
                 <div className='col-5 fs-25'><p>FINEST COFFEE <br/><span>LOCALLY CRAFTED</span></p></div>
