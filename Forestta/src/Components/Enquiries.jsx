b99361fa161595838e9e946270bc2d11095ada6d
Bryce <gichuhibryson09@gmail.com>
2025-02-01 18:04:33 +0300

    updates

--- Forestta/src/Components/Enquiries.jsx
+++ Forestta/src/Components/Enquiries.jsx
@@ -4,22 +4,28 @@ import './Enquiries.css'
 const Enquiries = () => {
   return (
     <div className='enquiries'>
-        <div className="cards" id='cards'>
-            <div className="card bg-light" id='card1' style={{width: '400px', height: '300px'}}>
-                <p>GRAB A MEAL IN THE COMFORT OF YOUR HOME.</p>
-                <button>ORDER NOW</button>
-            </div>
-            <div className="card" id='card2'>
-                <p>DINE WITH US AT OUR RESTAURANT.</p>
-                <button>FIND A TABLE</button>
-            </div>
+      <section>
+        <div className="" id='card1'>
+          <div className="cardbody">
+            <p className='fw-semibold'>GRAB A MEAL IN THE COMFORT OF YOUR HOME.</p>
+            <button>ORDER NOW</button>
+          </div>
         </div>
-          {/* <div className="card2">  <div className="card">
-                <div><img src="Item16.jpeg" alt="" /></div>
-                <div><p>PLANNING A LARGER GATHERING OR A PRIVATE EVENT?</p></div>
-                <button>ENQUIRE NOW</button>
-            </div>
-            </div> */}
+        <div className="" id='card2'>
+          <div className="cardbody">
+            <p className='fw-semibold'>DINE WITH US AT OUR RESTAURANT.</p>
+            <button>FIND TABLE</button>
+          </div>
+        </div>
+        </section>
+         <div className='d-flex' id='card3'>
+          <div className="cardbody">
+            <p>PLANNING A LARGER GATHERING OR A PRIVATE EVENT?</p>
+            <button>ENQUIRE NOW</button>
+            <img src="Item19.jpg" alt="" />
+          </div>
+         </div>
+
     </div>
   )
 }
