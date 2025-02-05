1935f0e09881a1be737560a6f9c8e5f92b988593
Bryce <gichuhibryson09@gmail.com>
2025-02-05 22:50:51 +0300

    updates

--- Forestta/src/App.jsx
+++ Forestta/src/App.jsx
@@ -1,29 +1,30 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import Headroom from 'react-headroom';
-import Navbar from './Components/Navbar'
-import Hero from './Components/Hero';
-import About from './Components/About';
-import Services from './Components/Services';
-import Information from './Components/Information';
+import 'bootstrap/dist/css/bootstrap.css'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Headroom from 'react-headroom'
+import Hero from './Pages/Home/Hero';
+import About from './Pages/Home/About';
+import Services from './Pages/Home/Services';
+import Information from './Pages/Home/Information';
 import { useState } from 'react';
-import Drinks from './Components/Drinks';
-import Enquiries from './Components/Enquiries';
-import Footer from './Components/Footer';
+import Drinks from './Pages/Home/Drinks';
+import Enquiries from './Pages/Home/Enquiries';
+import Footer from './Pages/Home/Footer';
+import Navbar from './Pages/Home/Navbar';
 
 const App = () => {
   const [imageCount, setImageCount] = useState(0);
   return (
     <>
-    <Headroom>
-      <Navbar />
-    </Headroom>
-    <Hero />
-    <About />
-    <Services />
-    <Information/> 
-    <Drinks />
-    <Enquiries />
-    <Footer />
+      <Headroom>
+        <Navbar />
+      </Headroom>
+      <Hero />
+      <About />
+      <Services />
+      <Information/> 
+      <Drinks />
+      <Enquiries />
+      <Footer />
     </>
   )
 }
--- Forestta/src/Pages/Home/Menu.jsx
+++ Forestta/src/Pages/Home/Menu.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './Navbar'
+import Navbar from '../Pages/Home/Navbar'
 
 const Menu = () => {
   return (
