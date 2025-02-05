276fa7c1de3bb8508c68914ab7a3fdd47130a45f
Bryce <gichuhibryson09@gmail.com>
2025-02-05 21:17:52 +0300

    Menu update

---
+++ Forestta/src/Components/Menu.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Menu = () => {
+  return (
+    <>
+    <Navbar />
+    <div>
+        <div>
+            <img src="Item21.jpg" alt="" />
+        </div>
+        <div>
+            <h2>WE WOULD LOVE TO <br />HAVE YOU DINE WITH US!</h2>
+            <p>Whether it’s an intimate celebration, a corporate lunch, or a special occasion, our restaurant offers the perfect ambiance and delectable dishes to make your experience truly memorable.</p>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Menu
